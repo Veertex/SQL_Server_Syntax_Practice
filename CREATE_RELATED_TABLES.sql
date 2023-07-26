7ddb6c265a9a1ba906af578fda2134a9166bdbc6
Williams Torres <williams.torres783@gmail.com>
2023-07-26 18:06:01 -0400

    fix(constraints): add a correctly separations of constraints on sentences

--- CREATE_RELATED_TABLES.sql
+++ CREATE_RELATED_TABLES.sql
@@ -7,8 +7,8 @@ CREATE TABLE categoria_productos(
 CREATE TABLE producto(
     id_producto INT IDENTITY(1,1) NOT NULL,
     nombre_producto VARCHAR NOT NULL,
-    id_categoria_producto INT NOT NULL
-	CONSTRAINT pk_id_producto PRIMARY KEY(id_producto)
+    id_categoria_producto INT NOT NULL,
+	CONSTRAINT pk_id_producto PRIMARY KEY(id_producto),
     CONSTRAINT id_categoria_producto FOREIGN KEY (id_categoria_producto)
-	REFERENCES categoria_productos (id_categoria_producto)
+	REFERENCES categoria_productos(id_categoria_producto)
 );
