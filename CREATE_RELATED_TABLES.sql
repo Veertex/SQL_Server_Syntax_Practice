9b5d35aa1a172ef2b47df43ad67a0903a57117f6
Williams Torres <williams.torres783@gmail.com>
2023-07-26 19:05:36 -0400

    refactor(): add max values for varchar data types when table creates

--- CREATE_RELATED_TABLES.sql
+++ CREATE_RELATED_TABLES.sql
@@ -1,14 +1,14 @@
 CREATE TABLE categoria_productos(
 	id_categoria_producto INT IDENTITY(1,1) NOT NULL,
-	nombre_categoria VARCHAR NOT NULL,
+	nombre_categoria VARCHAR(256) NOT NULL,
 	CONSTRAINT pk_id_categoria_producto PRIMARY KEY(id_categoria_producto)
 );
 
 CREATE TABLE producto(
 	id_producto INT IDENTITY(1,1) NOT NULL,
-	nombre_producto VARCHAR NOT NULL,
+	nombre_producto VARCHAR(256) NOT NULL,
 	id_categoria_producto INT NOT NULL,
 	CONSTRAINT pk_id_producto PRIMARY KEY(id_producto),
-	CONSTRAINT id_categoria_producto FOREIGN KEY (id_categoria_producto)
+	CONSTRAINT fk_id_categoria_producto FOREIGN KEY (id_categoria_producto)
 	REFERENCES categoria_productos(id_categoria_producto)
 );
